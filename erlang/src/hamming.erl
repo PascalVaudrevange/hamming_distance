e4521d7556cb601c862570f7920ee53fb57c26b6
Pascal Vaudrevange <pascal.vaudrevange@d-fine.de>
2021-04-02 06:41:16 +0000

    fixed binary conversion

--- erlang/src/hamming.erl
+++ erlang/src/hamming.erl
@@ -1,5 +1,5 @@
 -module(hamming).
--export([hamming_distance/2]).
+-export([hamming_distance/2, popcount/1]).
 -include_lib("eunit/include/eunit.hrl").
 
 popcount(N) -> 
@@ -11,9 +11,19 @@ popcount(N, Acc) ->
     popcount(N div 2, Acc + N rem 2).
 
 hamming_distance(X, Y) -> 
-    Xb = binary:bin_to_list(unicode:characters_to_binary(X)),
-    Yb = binary:bin_to_list(unicode:characters_to_binary(Y)),
+    Xb = binary:decode_unsigned(
+        unicode:characters_to_binary(X)
+    ),
+    Yb = binary:decode_unsigned(
+        unicode:characters_to_binary(Y)
+    ),
     popcount(Xb bxor Yb).
+
+popcount_0_test() -> 0 = popcount(0).
+popcount_1_test() -> 1 = popcount(1).
+popcount_2_test() -> 1 = popcount(2).
+popcount_3_test() -> 2 = popcount(3).
+popcount_4_test() -> 1 = popcount(4).
 
 hamming_distance_0_test() -> 0 = hamming:hamming_distance("123", "123").
 hamming_distance_1_test() -> 1 = hamming:hamming_distance("023", "123").
